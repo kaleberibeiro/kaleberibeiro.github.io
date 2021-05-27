27f6c63d5da2c93f5b40e342a28e6a64c37e24a5
kalebe <kaleberibeiro@ua.pt>
2021-05-27 11:33:08 +0100

    projeto

---
+++ ProjetoCV/scripts/main.js
@@ -0,0 +1,15 @@
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+
+var NomeTitulo = document.getElementsByClassName("site-title mb-0");
+
+fetch('cv.json')
+        .then(response => response.json())
+        .then(function(cv){
+          NomeTitulo.innerHTML = cv.Nome;
+        })
