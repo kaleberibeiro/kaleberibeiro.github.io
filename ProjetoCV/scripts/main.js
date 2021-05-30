5a35a45fe0784c3dbeebf28337670b6196e494c3
kalebe <kaleberibeiro@ua.pt>
2021-05-30 11:46:08 +0100

    portfolio

--- ProjetoCV/scripts/main.js
+++ ProjetoCV/scripts/main.js
@@ -17,6 +17,7 @@ var Morada = document.getElementById("morada");
 var fotoPerfil = document.getElementById("imagemPerfil");
 var sobre = document.getElementById("sobreMim");
 var Educacao = document.getElementById("Educacao"); 
+var Portfolio = document.getElementById("portfolio"); 
 
 fetch('cv.json')
         .then(response => response.json())
@@ -32,21 +33,30 @@ fetch('cv.json')
           fotoPerfil.src = cv.geral.Foto;
           sobre.innerText = cv.geral.Descrição;
 
-
-          /*cv.forEach(function(e){
-            console.log("Curso: " + e.Curso);
-            console.log("Escola: " + e.Escola);
-            console.log("Epoca: " + e.Epoca);
-          })*/
           
+
+          //educação
           for( var e in cv.Educacao){
             Educacao.innerHTML += "<div class='timeline-card timeline-card-success' data-aos='fade-in' data-aos-delay='0'>"+
-          "<div class='timeline-head px-4 pt-3'>"+ "<div class='h5'>" + cv.Educacao[e].Curso + "<span class='text-muted h6'>"+ cv.Educacao[e].Escola +"</span>          </div>"+
-          "</div>"+
-          "<div class='timeline-body px-4 pb-4'>"+
+            "<div class='timeline-head px-4 pt-3'>"+ "<div class='h5'>" + cv.Educacao[e].Curso + "<span class='text-muted h6'>"+ cv.Educacao[e].Escola +"</span>          </div>"+
+            "</div>"+
+            "<div class='timeline-body px-4 pb-4'>"+
             "<div class='text-muted text-small mb-3'>"+ cv.Educacao[e].Epoca + "</div>" +
-          "</div>"+
-        "</div>";
+            "</div>"+
+            "</div>";
           }
-          //educação
+
+          //portfolio
+          for( var p in cv.Portfolio){
+            Portfolio.innerHTML += "<div class= 'row g-0'> " +
+            "<div class='col-md-6'>" + "<img class='img-fluid' src='"+cv.Portfolio[p].Foto + "' width='800' height='500'/>"+"</div>"+
+            "<div class='col-md-6 d-flex align-items-center' data-aos='fade-left' data-aos-delay='100'>"+
+              "<div class='m-4 mt-md-2'>" +
+                "<p class='text-teal text-small'>" + cv.Portfolio[p].Linguas +"</p>" +
+                "<h3>"+ cv.Portfolio[p].Titulo +"</h3>"+
+                "<p class='text-muted'>"+cv.Portfolio[p].Descricao +"</p>"+
+              "</div>"+
+            "</div>"+
+          "</div>"
+          }
         })
